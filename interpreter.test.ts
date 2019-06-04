df214e78c5969841f0adc9639b9708d60e557ec0
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-06-04 19:39:47 +0300

    Implement increment and decrement operations

--- interpreter.test.ts
+++ interpreter.test.ts
@@ -1,10 +1,36 @@
 import * as _mocha from "mocha";
 import { expect } from "chai";
-import { load } from "./interpreter";
+import { load, execute } from "./interpreter";
 
 describe("Interpreter", () => {
-  it("should do stuff", () => {
-    const program = "[]";
-    expect(program).to.equal(load(program));
+  it("should increment byte by one", () => {
+    let system = load("+");
+    expect(system.cells[0]).to.equal(0);
+    execute(system);
+    expect(system.cells[0]).to.equal(1);
+    expect(system.pointers.instruction).to.equal(1);
+  });
+  it("should decrement byte by one", () => {
+    let system = load("-");
+    system.cells[0] = 1;
+    expect(system.cells[0]).to.equal(1);
+    execute(system);
+    expect(system.cells[0]).to.equal(0);
+    expect(system.pointers.instruction).to.equal(1);
+  });
+  it("should move data pointer forward by one", () => {
+    let system = load(">");
+    expect(system.pointers.data).to.equal(0);
+    execute(system);
+    expect(system.pointers.data).to.equal(1);
+    expect(system.pointers.instruction).to.equal(1);
+  });
+  it("should move data pointer backwards by one", () => {
+    let system = load("<");
+    system.pointers.data = 1;
+    expect(system.pointers.data).to.equal(1);
+    execute(system);
+    expect(system.pointers.data).to.equal(0);
+    expect(system.pointers.instruction).to.equal(1);
   });
 });
--- interpreter.ts
+++ interpreter.ts
@@ -1,3 +1,50 @@
-export function load(program: string): string {
+interface System {
+  cells: Uint8Array;
+  pointers: {
+    instruction: number;
+    data: number;
+  };
+  program: string;
+}
+
+export function strip(program: string): string {
+  // TODO: Strip comments
   return program;
+}
+
+export function load(program: string): System {
+  const strippedProgram = strip(program);
+  return {
+    cells: new Uint8Array(30_000),
+    program: strippedProgram,
+    pointers: {
+      data: 0,
+      instruction: 0
+    }
+  };
+}
+
+export function dump(system: System): void {
+  console.log(system);
+}
+
+export function execute(system: System): void {
+  if (system.pointers.instruction >= system.program.length) {
+    throw new Error("Exceeded program bounds");
+  }
+
+  const instruction = system.program[system.pointers.instruction];
+  if (instruction === "+") {
+    system.cells[system.pointers.data]++;
+    system.pointers.instruction++;
+  } else if (instruction === "-") {
+    system.cells[system.pointers.data]--;
+    system.pointers.instruction++;
+  } else if (instruction === ">") {
+    system.pointers.data++;
+    system.pointers.instruction++;
+  } else if (instruction === "<") {
+    system.pointers.data--;
+    system.pointers.instruction++;
+  }
 }
